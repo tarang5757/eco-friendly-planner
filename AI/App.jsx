37cfbe68a61b8a547293930fd2dc1b01bc70de53
Luana Madeira <105006518+Luanarm@users.noreply.github.com>
2023-10-17 10:46:22 -0400

    Update App.jsx

--- AI/App.jsx
+++ AI/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react'
 
-const API_KEY = "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";
+const API_KEY = "";
 
 function App() {
   const [typing, setTyping] = useState(false);
